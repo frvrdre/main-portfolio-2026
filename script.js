0e0795e11a1346d0b2ccfc2161885365294a93a0
dre <100737250+frvrdre@users.noreply.github.com>
2026-07-18 18:58:22 +0000

    aquarium startup added and fixed visibility of btn

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-// GAME STATE
+// AQUARIUM STATE
 
 let clickCount = 0;
-let score = 0;
-let lives = 3;
-let isPlaying = false;
-let playerX = 50;
+let isAquariumActive = false;
+
+// ARRAYS
+
+const fish = [];
+const bubbles = [];
 
 // DOM ELEMENTS
 
 const chaosBtn = document.getElementById("chaos-btn");
-const exitBtn = document.getElementById("exit-btn");
+const returnBtn = document.getElementById("return-btn");
 
-const portfolioScrn = document.getElementById("portfolio");
-const gameScrn = document.getElementById("game-screen");
+const aquariumMode = document.getElementById("aquarium-mode");
+const waterLayer = document.getElementById("water-layer");
+const fishLayer = document.getElementById("fish-layer");
+const bubbleLayer = document.getElementById("bubble-layer");
+const constructTxt = document.getElementById("under-construct");
+const portfolioMode = document.getElementById("portfolio");
 
 const achievementPop = document.getElementById("achievement");
 
 const pingAudio = document.getElementById("ping");
+
+// AQUARIUM
+
+// I will build these later:
+//
+// startAquarium()
+// createFish()
+// createBubble()
+// moveFish()
+// stopAquarium()
 
 // ACHIEVEMENT
 
@@ -24,21 +40,22 @@ function clickChaos() {
   clickCount++;
 
   if (clickCount === 1) {
-    chaosBtn.innerHTML = "I SAID IGNORE ME";
+    chaosBtn.innerHTML = "I SAID DONT CLICK ME";
   } else if (clickCount === 2) {
     chaosBtn.innerHTML = "YOU ASKED FOR THIS";
   } else if (clickCount === 3) {
-    gameScrn.classList.remove("hidden");
-    portfolioScrn.classList.add("hidden");
-
-    isPlaying = true;
-
+    aquariumMode.classList.remove("hidden");
+    constructTxt.classList.remove("hidden");
+    portfolioMode.classList.add("dim-down");
+    isAquariumActive = true;
+    
     chaosBtn.classList.add("hidden");
+
     achievementPop.classList.remove("hidden");
     achievementPop.classList.add("fade-in");
     achievementPop.classList.add("shake");
 
-    pingAudio.volume = 0.05;
+    pingAudio.volume = 0.3;
     pingAudio.play();
 
     setTimeout(notiRemove, 1000);
@@ -50,27 +67,33 @@ function clickChaos() {
 function notiRemove() {
   achievementPop.classList.add("fade-out");
   achievementPop.classList.remove("shake");
-  achievementPop.classList.add("fade-out");
 
   setTimeout(() => {
     achievementPop.classList.add("hidden");
   }, 3200);
 }
 
-// EXIT GAME
+// RETURN TO NORMAL PORTFOLIO
 
-function exitGame() {
-  gameScrn.classList.add("hidden");
-  portfolioScrn.classList.remove("hidden");
-
+function returnToPortfolio() {
+  aquariumMode.classList.add("hidden");
+  constructTxt.classList.add("hiddem");
   chaosBtn.classList.remove("hidden");
+  portfolioMode.classList.remove("dim-down");
+
   chaosBtn.innerHTML = "IGNORE ME";
 
   clickCount = 0;
-  isPlaying = false;
+  isAquariumActive = false;
+
+  // Later you will:
+  // remove fish
+  // remove bubbles
+  // stop animations
+  // reset aquarium state
 }
 
 // EVENT LISTENERS
 
 chaosBtn.addEventListener("click", clickChaos);
-exitBtn.addEventListener("click", exitGame);
+returnBtn.addEventListener("click", returnToPortfolio);
